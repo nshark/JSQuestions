1a9c046eca249707d78e0cee5cd2a783a8ef9986
nshark <noah@sitkoff.net>
2023-10-16 12:25:59 -0400

    Doneness

--- jsquestion.js
+++ jsquestion.js
@@ -1,7 +1,9 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+const { randomInt } = require("firebase-tools/lib/utils");
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min;
   }
-var c = getRandomInt(2147483648);
+var c = getRandomInt(0, 2147483648);
 console.log("Q1: " + c);
 if ((c % 7 == 0) && (c % 9 == 0)){
     console.log("Q1: Y");
@@ -26,4 +28,36 @@ console.log("Q3: " + rocheLimit(6378.1, 5513, 3346))
 const rocheLimitArrow = (p3, p2, p1) => {
     return rocheLimit(p1,p2,p3);
 };
-console.log("Q4: " + rocheLimitArrow(getRandomInt(10000), getRandomInt(10000), getRandomInt(10000)))
+console.log("Q4: " + rocheLimitArrow(getRandomInt(0, 10000), getRandomInt(0, 10000), getRandomInt(0, 10000)))
+var c = ["1234asdf","32sadf", "asdfmkl", "vmdfjv", "zxcvczxkl", "iozxcv"];
+console.log("Q5: len: " + c.length + " first:" + c[0] + " last:" + c[c.length-1] + " random: " + c[randomInt(0, c.length-1)]);
+c = c.sort();
+c.push("suffering");
+c.push(243);
+k = [];
+for (let m = 0; m < 10; m++){
+    k.push(randomInt(29, 87));   
+}
+min = 214
+max = 0
+k.forEach(element => {
+    console.log("Q6:" + element);
+    if(element > max){
+        max = element;
+    }
+    if(element < min){
+        min = element;
+    }
+});
+console.log("Q6: " + (max-min));
+k = k.map(element =>
+    Math.round(Math.sqrt(element)));
+console.log("Q7:" + k);
+kmax = 0;
+k.forEach(element => {if(element>kmax){kmax=element}})
+k = k.filter((element) => element >= kmax/2);
+console.log("Q8:" + k);
+console.log("Q9: " + k.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+k.forEach((element, index) => {
+    console.log("Q10: k[" + index + "] = " + element);
+})
